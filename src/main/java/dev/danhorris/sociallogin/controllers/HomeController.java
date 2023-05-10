90fa30c32e1bce6f1cf3e26c3249cf612cb6f561
Daniel Horrisberger <dhorri@mecon.gob.ar>
2023-05-10 13:54:23 -0300

    github login

---
+++ src/main/java/dev/danhorris/sociallogin/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package dev.danhorris.sociallogin.controllers;
+
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello Home";
+    }
+
+    
+    @GetMapping("/secure")
+    public String secure(OAuth2AuthenticationToken principal){
+        return "Hello Secured";
+    }
+    
+}
